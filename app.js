02c8ccd70eb822a21722ec11210c643e874f0413
Jiheon Park <jhpark.a@imagoworks.ai>
2023-11-24 11:01:54 +0900

    feat: openai api

--- app.js
+++ app.js
@@ -2,18 +2,38 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 const cors = require("cors");
+const OpenAI = require("openai");
+require("dotenv").config();
 
 const app = express();
-
 app.use(cors());
+app.use(express.json());
 
 const server = http.createServer(app);
-
 const io = socketIo(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
+  cors: { origin: "*", methods: ["GET", "POST"] },
+});
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+  organization: process.env.OPENAI_ORGANIZATION_ID,
+});
+
+app.post("/openai", async (req, res) => {
+  const { prompt } = req.body;
+
+  try {
+    const response = await openai.createCompletion({
+      model: "text-davinci-002",
+      prompt: prompt,
+      max_tokens: 150,
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while processing your request.");
+  }
 });
 
 io.on("connection", (socket) => {
